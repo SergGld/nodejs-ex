8cd89b29e597ef63383603146d85980ca9631234
SergGld <spg4526@gmail.com>
2017-12-16 10:37:47 +0300

    xdd

--- server.js
+++ server.js
@@ -65,30 +65,30 @@ var initDb = function (callback) {
 app.get('/', function (req, res) {
     // try to initialize the db on every request if it's not already
     // initialized.
-    if (!db) {
-        initDb(function (err) {});
-    }
-    if (db) {
-        var col = db.collection('counts');
-        // Create a document with request IP and current time of request
-        col.insert({
-            ip: req.ip,
-            date: Date.now()
-        });
-        col.count(function (err, count) {
-            if (err) {
-                console.log('Error running count. Message:\n' + err);
-            }
-            res.render('index.html', {
-                pageCountMessage: collection,
-                dbInfo: dbDetails
-            });
-        });
-    } else {
+//    if (!db) {
+//        initDb(function (err) {});
+//    }
+//    if (db) {
+//        var col = db.collection('counts');
+//        // Create a document with request IP and current time of request
+//        col.insert({
+//            ip: req.ip,
+//            date: Date.now()
+//        });
+//        col.count(function (err, count) {
+//            if (err) {
+//                console.log('Error running count. Message:\n' + err);
+//            }
+//            res.render('index.html', {
+//                pageCountMessage: collection,
+//                dbInfo: dbDetails
+//            });
+//        });
+//    } else {
         res.render('index.html', {
             pageCountMessage: 'elsed'
         });
-    }
+
 });
 app.get('/feed', function (req, res) {
     // try to initialize the db on every request if it's not already
